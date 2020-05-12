708d262bc41878af53d26800737bd22f8a8b3edf
Gera Petrov <vg14041994@gmail.com>
2020-05-12 11:13:37 +0300

    Add task
    Add best task

---
+++ Maximum subarray sum/Maximum subarray sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Maximum_subarray_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+            Console.WriteLine(Kata.MaxSequence(arr));
+            Console.ReadKey();
+        }
+    }
+    public static class Kata
+    {
+        public static int MaxSequence(int[] arr)
+        {
+            int max = 0, res = 0, sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+                max = sum > max ? max : sum;
+                res = res > sum - max ? res : sum - max;
+            }
+            return res;
+        }
+    }
+}
